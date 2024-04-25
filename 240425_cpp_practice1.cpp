f2871f5997417e0de09819b3e15bb637c95c7a21
dlskrud815 <dlskrud815@gmail.com>
2024-04-25 15:44:04 +0900

    입력단, 입력 판별, 입력 처리 구분

--- 240425_cpp_practice1.cpp
+++ 240425_cpp_practice1.cpp
@@ -18,56 +18,24 @@ int main()
 
 	while (1)
 	{
-		cout << endl << "나이를 입력해주세요 (-1 입력시 종료)" << endl;
+		cout << endl << "나이를 입력해주세요." << endl;
 		cin >> age;
-
-		// type
-		// 0. -1 입력 시 종료
-		// 1. 배열 내 입력한 나이 존재
-		// 2. 배열 내 입력한 나이 부재
-
-		int type = 0;
-
 
 		if (age == -1)
 		{
-			type = 0;
+			break;
 		}
-		else 
+		else
 		{
 			for (int i = 1; i < numRows; i++)
 			{
 				if (age == stoi(score[i][1]))
 				{
-					type = 1;
-					break;
-				}
-				else
-				{
-					type = 2;
+					cout << "이름: " << score[i][0] << ", 수학 점수: " << score[i][3] << endl;
 				}
 			}
-		}
-
-		switch (type)
-		{
-		case 0:
-			cout << "프로그램을 종료합니다." << endl;
-			return 0;
-
-		case 1:
-			for (int j = 1; j < numRows; j++)
-			{
-				if (age == stoi(score[j][1]))
-				{
-					cout << "이름: " << score[j][0] << ", 수학 점수: " << score[j][3] << endl;
-				}
-			}
-			break;
-
-		case 2:
-			cout << "해당 나이는 존재하지 않습니다." << endl;
-			break;
 		}
 	}
+
+	return 0;
 }
