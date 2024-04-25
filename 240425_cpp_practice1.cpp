7de5754b2f548ad330a8ff9e47cc8059ef82f99d
dlskrud815 <dlskrud815@gmail.com>
2024-04-25 15:25:03 +0900

    add space

--- 240425_cpp_practice1.cpp
+++ 240425_cpp_practice1.cpp
@@ -1,41 +1,10 @@
 ﻿#include <iostream>
-#include<string>
 using namespace std;
 
 int main()
 {
-	string score[5][4] = {
-		{"이름", "나이", "국어", "수학"},
-		{"방지턱", "19", "66", "68"},
-		{"정직한", "20", "73", "94"},
-		{"김가루", "19", "58", "37"},
-		{"조나단", "20", "82", "29"}
+	std::string fruit[2][2] = { //2차원, 3차원=> [2][2][2], n차원=> [2][2]...[2]
+	{"apple", "banana"},
+	{"orange", "strawberry"}
 	};
-
-	int numRows = sizeof(score) / sizeof(score[0]);
-	int numCols = sizeof(score[0]) / sizeof(score[0][0]);
-	int age;
-
-	while (1)
-	{
-		cout << endl << "나이를 입력해주세요." << endl;
-		cin >> age;
-
-		if (age == -1)
-		{
-			break;
-		}
-		else
-		{
-			for (int i = 1; i < numRows; i++)
-			{
-				if (age == stoi(score[i][1]))
-				{
-					cout << "이름: " << score[i][0] << ", 수학 점수: " << score[i][3] << endl;
-				}
-			}
-		}
-	}
-	
-	return 0;
 }
