8ecafadfb0718c06e859428fe9192ab654b2dde2
dlskrud815 <dlskrud815@gmail.com>
2024-04-25 18:13:44 +0900

    Exercise3

---
+++ Exercise3.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+#define ARRAY_SIZE 5
+
+void reverse(int* array)
+{
+
+    for (int i = 0; i < ARRAY_SIZE / 2; i++) {
+        int temp = *(array + i);
+        *(array + i) = *(array + (ARRAY_SIZE - i - 1));
+        *(array + (ARRAY_SIZE - i - 1)) = temp;
+    }
+}
+
+int main()
+{
+	int myArray[] = { 1, 2, 3, 4, 5 };
+
+	reverse(&myArray[0]);
+
+	for (int k = 0; k < ARRAY_SIZE; k++)
+	{
+		cout << myArray[k] << endl;
+	}
+
+	return 0;
+}
